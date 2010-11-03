eeb6266cd32b0c3906a30aa0d57264d84c49deeb
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2010-11-03 13:41:31 +0000

    Populated settings.py with default values

---
+++ settings.py
@@ -0,0 +1,9 @@
+
+#Url of irc server than l33ty should run on
+IRC_HOST = "irc.freenode.net"
+
+#Port of irc server that l33ty should run on
+IRC_PORT = 6667
+
+#Channels that l33ty should be in
+IRC_CHANNELS = ("#testingbot",)
